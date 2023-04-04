5aca6a3606e309ddc7a246fced89a0e26b379215
Naperville Nate <napervillepolitics@gmail.com>
2023-04-04 14:05:01 -0500

    init runoff

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,15 @@
 var geojson;
 var geojson2;
 var toggle = "Precinct";
+var reset_toggle=""
 var map = L.map("map").setView([41.8781, -87.6298], 11.3);
-var Green_Total = 0,
-  King_Total = 0,
-  Buckner_Total = 0,
-  Wilson_Total = 0,
+var
+
+ 
+
   Johnson_Total = 0,
   Vallas_Total = 0,
-  Lightfoot_Total = 0,
-  Sawyer_Total = 0,
-  Garcia_Total = 0,
+  
   Votes_Total = 0,
   Winner = "Tie";
 
@@ -34,7 +33,9 @@ const response = fetch("./Outputs/Precincts_Output.geojson")
     });
 
     map.addLayer(geojson);
+    reset_toggle= Winner;
     reset();
+    
   });
 
 const response2 = fetch("./Outputs/Wards_Output.geojson")
@@ -60,6 +61,10 @@ function highlightFeature(e) {
   if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
     layer.bringToFront();
   }
+
+
+  
+ 
 }
 function style(feature) {
   return {
@@ -71,11 +76,11 @@ function style(feature) {
   };
 }
 function resetHighlight(e) {
+  
   geojson.resetStyle(e.target);
-
   highlight_reset_row(e.target.feature.properties.Winner);
-
   reset();
+
 }
 
 //function zoomToFeature(e) {
@@ -91,48 +96,12 @@ function onEachFeature(feature, layer) {
 }
 
 function getColor(d,c) {
-  return d == "Green"
-    ? choropleth(c,'#e3342f','#871612','#b41e18','#e0251f','#e7514b','#ed7c78,','#f3a8a5','#f9d3d2')
-    : d == "King"
-    ? choropleth(c,'#613305','#914d08','#c2660a','#f2800d','#f5993d','#f7b36e','#facc9e','#fce6cf')
-    : d == "Buckner"
-    ? choropleth(c,'#665c00','#998a00','#ccb800','#ffe600','#ffeb33','#fff066','#fff599','#fffacc')
-    : d == "Wilson"
-    ? choropleth(c,'#174f2e','#227745','#2d9f5c','#38c774','#60d28f','#88ddab','#afe9c7','#d7f4e3')
-    : d == "Johnson"
+  return d == "Johnson"
     ? choropleth(c,'#4dc0b5','#277269','#34988c','#41beaf','#67cbbf','#8dd8cf','#b3e5df','#d9f2ef')
     : d == "Vallas"
-    ? choropleth(c,'#0c365a','#125087','#186bb4','#1f86e0','#4b9ee7','#78b6ed','#a5cff3','#d2e7f9')
-    : d == "Lightfoot"
-    ? choropleth(c,'#19214d','#253174','#31419b','#3d51c2','#6474ce','#8b97da','#b1b9e7','#d8dcf3')
-    : d == "Sawyer"
-    ? choropleth(c,'#2c1056','#421881','#5820ac','#6e28d7','#8b53df','#a87ee7','#c5a9ef','#e2d4f7')
-    : d == "Garcia"
     ? choropleth(c,'#600624','#900936','#c00c48','#f00f5a','#f33f7b','#f66f9c','#f99fbd','#fccfde')
     : "#ccc9c0";
 }
-
-/* old colors
- return d == "Green"
-    ? "#e3342f"
-    : d == "King"
-    ? "#f6993f"
-    : d == "Buckner"
-    ? "#ffed4a"
-    : d == "Wilson"
-    ? "#38c172"
-    : d == "Johnson"
-    ? "#4dc0b5"
-    : d == "Vallas"
-    ? "#3490dc"
-    : d == "Lightfoot"
-    ? "#6574cd"
-    : d == "Sawyer"
-    ? "#9561e2"
-    : d == "Garcia"
-    ? "#f66d9b"
-    : "#ccc9c0";
-*/
 
 choropleth =function(d,c1,c2,c3,c4,c5,c6,c7,c8){
   return d > 0.5
@@ -166,30 +135,7 @@ info = function (props) {
       "W" + props.Ward.padStart(2, "0");
   }
 
-  document.getElementById("Green_votes").innerHTML =
-    props.Green.toLocaleString("en-US");
-  document.getElementById("Green_percent").innerHTML = (
-    divide(props.Green, props.Votes) * 100
-  ).toFixed(2);
-
-  document.getElementById("King_votes").innerHTML =
-    props.King.toLocaleString("en-US");
-  document.getElementById("King_percent").innerHTML = (
-    divide(props.King, props.Votes) * 100
-  ).toFixed(2);
-
-  document.getElementById("Buckner_votes").innerHTML =
-    props.Buckner.toLocaleString("en-US");
-  document.getElementById("Buckner_percent").innerHTML = (
-    divide(props.Buckner, props.Votes) * 100
-  ).toFixed(2);
-
-  document.getElementById("Wilson_votes").innerHTML =
-    props.Wilson.toLocaleString("en-US");
-  document.getElementById("Wilson_percent").innerHTML = (
-    divide(props.Wilson, props.Votes) * 100
-  ).toFixed(2);
-
+  
   document.getElementById("Johnson_votes").innerHTML =
     props.Johnson.toLocaleString("en-US");
   document.getElementById("Johnson_percent").innerHTML = (
@@ -202,53 +148,35 @@ info = function (props) {
     divide(props.Vallas, props.Votes) * 100
   ).toFixed(2);
 
-  document.getElementById("Lightfoot_votes").innerHTML =
-    props.Lightfoot.toLocaleString("en-US");
-  document.getElementById("Lightfoot_percent").innerHTML = (
-    divide(props.Lightfoot, props.Votes) * 100
-  ).toFixed(2);
-
-  document.getElementById("Sawyer_votes").innerHTML =
-    props.Sawyer.toLocaleString("en-US");
-  document.getElementById("Sawyer_percent").innerHTML = (
-    divide(props.Sawyer, props.Votes) * 100
-  ).toFixed(2);
-
-  document.getElementById("Garcia_votes").innerHTML =
-    props.Garcia.toLocaleString("en-US");
-  document.getElementById("Garcia_percent").innerHTML = (
-    divide(props.Garcia, props.Votes) * 100
-  ).toFixed(2);
-
+  if(props.Vallas > props.Johnson){
+    var content = document.getElementById('Vallas_row');
+    var parent = content.parentNode;
+    parent.insertBefore(content, parent.firstChild.nextSibling);
+  } else if(props.Johnson > props.Vallas){
+    var content = document.getElementById('Johnson_row');
+    var parent = content.parentNode;
+    parent.insertBefore(content, parent.firstChild.nextSibling);
+  }
+  
+  if(props.Vallas == props.Johnson){
+    if(reset_toggle=="Vallas"){
+      var content = document.getElementById('Vallas_row');
+      var parent = content.parentNode;
+      parent.insertBefore(content, parent.firstChild.nextSibling);
+    }else{
+      var content = document.getElementById('Johnson_row');
+      var parent = content.parentNode;
+      parent.insertBefore(content, parent.firstChild.nextSibling);
+    }
+  }
   highlight_row(props.Winner);
+  reset_toggle=props.Winner;
 };
 
 reset = function () {
+ 
+  
   document.getElementById("Name").innerHTML = "Total";
-
-  document.getElementById("Green_votes").innerHTML =
-    Green_Total.toLocaleString("en-US");
-  document.getElementById("Green_percent").innerHTML = (
-    divide(Green_Total, Votes_Total) * 100
-  ).toFixed(2);
-
-  document.getElementById("King_votes").innerHTML =
-    King_Total.toLocaleString("en-US");
-  document.getElementById("King_percent").innerHTML = (
-    divide(King_Total, Votes_Total) * 100
-  ).toFixed(2);
-
-  document.getElementById("Buckner_votes").innerHTML =
-    Buckner_Total.toLocaleString("en-US");
-  document.getElementById("Buckner_percent").innerHTML = (
-    divide(Buckner_Total, Votes_Total) * 100
-  ).toFixed(2);
-
-  document.getElementById("Wilson_votes").innerHTML =
-    Wilson_Total.toLocaleString("en-US");
-  document.getElementById("Wilson_percent").innerHTML = (
-    divide(Wilson_Total, Votes_Total) * 100
-  ).toFixed(2);
 
   document.getElementById("Johnson_votes").innerHTML =
     Johnson_Total.toLocaleString("en-US");
@@ -262,24 +190,17 @@ reset = function () {
     divide(Vallas_Total, Votes_Total) * 100
   ).toFixed(2);
 
-  document.getElementById("Lightfoot_votes").innerHTML =
-    Lightfoot_Total.toLocaleString("en-US");
-  document.getElementById("Lightfoot_percent").innerHTML = (
-    divide(Lightfoot_Total, Votes_Total) * 100
-  ).toFixed(2);
-
-  document.getElementById("Sawyer_votes").innerHTML =
-    Sawyer_Total.toLocaleString("en-US");
-  document.getElementById("Sawyer_percent").innerHTML = (
-    divide(Sawyer_Total, Votes_Total) * 100
-  ).toFixed(2);
-
-  document.getElementById("Garcia_votes").innerHTML =
-    Garcia_Total.toLocaleString("en-US");
-  document.getElementById("Garcia_percent").innerHTML = (
-    divide(Garcia_Total, Votes_Total) * 100
-  ).toFixed(2);
   highlight_row(Winner);
+
+  if(Vallas_Total > Johnson_Total){
+    var content = document.getElementById('Vallas_row');
+    var parent = content.parentNode;
+    parent.insertBefore(content, parent.firstChild.nextSibling);
+  } else if(Johnson_Total > Vallas_Total){
+    var content = document.getElementById('Johnson_row');
+    var parent = content.parentNode;
+    parent.insertBefore(content, parent.firstChild.nextSibling);
+  } 
 };
 
 ward_click = function () {
@@ -321,41 +242,16 @@ divide = function (var1, var2) {
 
 calc_total = function (response) {
   Votes_Total = response.Votes;
-  Green_Total = response.Green;
-  King_Total = response.King;
-  Buckner_Total = response.Buckner;
-  Wilson_Total = response.Wilson;
+  
+ 
   Johnson_Total = response.Johnson;
   Vallas_Total = response.Vallas;
-  Lightfoot_Total = response.Lightfoot;
-  Sawyer_Total = response.Sawyer;
-  Garcia_Total = response.Garcia;
+ 
   Winner = response.Winner;
 };
 
 highlight_row = function (feature) {
   switch (feature) {
-    case "Green":
-      document.getElementById("Green_row").style.fontWeight = "bold";
-      document.getElementById("Green_block").style.backgroundColor = "#e3342f";
-      document.getElementById("Green_row").style.color = "#fff";
-      break;
-    case "King":
-      document.getElementById("King_row").style.fontWeight = "bold";
-      document.getElementById("King_block").style.backgroundColor = "#f6993f";
-      document.getElementById("King_row").style.color = "#fff";
-      break;
-    case "Buckner":
-      document.getElementById("Buckner_row").style.fontWeight = "bold";
-      document.getElementById("Buckner_block").style.backgroundColor =
-        "#ffed4a";
-      document.getElementById("Buckner_row").style.color = "#fff";
-      break;
-    case "Wilson":
-      document.getElementById("Wilson_row").style.fontWeight = "bold";
-      document.getElementById("Wilson_block").style.backgroundColor = "#38c172";
-      document.getElementById("Wilson_row").style.color = "#fff";
-      break;
     case "Johnson":
       document.getElementById("Johnson_row").style.fontWeight = "bold";
       document.getElementById("Johnson_block").style.backgroundColor =
@@ -364,64 +260,19 @@ highlight_row = function (feature) {
       break;
     case "Vallas":
       document.getElementById("Vallas_row").style.fontWeight = "bold";
-      document.getElementById("Vallas_block").style.backgroundColor = "#3490dc";
+      document.getElementById("Vallas_block").style.backgroundColor = "#f66d9b";
       document.getElementById("Vallas_row").style.color = "#fff";
-      break;
-    case "Lightfoot":
-      document.getElementById("Lightfoot_row").style.fontWeight = "bold";
-      document.getElementById("Lightfoot_block").style.backgroundColor =
-        "#6574cd";
-      document.getElementById("Lightfoot_row").style.color = "#fff";
-      
-      break;
-    case "Sawyer":
-      document.getElementById("Sawyer_row").style.fontWeight = "bold";
-      document.getElementById("Sawyer_block").style.backgroundColor = "#9561e2";
-      document.getElementById("Sawyer_row").style.color = "#fff";
-      break;
-    case "Garcia":
-      document.getElementById("Garcia_row").style.fontWeight = "bold";
-      document.getElementById("Garcia_block").style.backgroundColor = "#f66d9b";
-      document.getElementById("Garcia_row").style.color = "#fff";
       break;
   }
 };
 
 highlight_reset_row = function (Winner) {
-  document.getElementById("Green_row").style.fontWeight = "normal";
-  document.getElementById("Green_block").style.backgroundColor = "#e3342f99";
-  document.getElementById("Green_row").style.color = "#ffffff80";
-
-  document.getElementById("King_row").style.fontWeight = "normal";
-  document.getElementById("King_block").style.backgroundColor = "#f6993f99";
-  document.getElementById("King_row").style.color = "#ffffff80";
-
-  document.getElementById("Buckner_row").style.fontWeight = "normal";
-  document.getElementById("Buckner_block").style.backgroundColor = "#ffed4a99";
-  document.getElementById("Buckner_row").style.color = "#ffffff80";
-
-  document.getElementById("Wilson_row").style.fontWeight = "normal";
-  document.getElementById("Wilson_block").style.backgroundColor = "#38c17299";
-  document.getElementById("Wilson_row").style.color = "#ffffff80";
 
   document.getElementById("Johnson_row").style.fontWeight = "normal";
   document.getElementById("Johnson_block").style.backgroundColor = "#4dc0b599";
   document.getElementById("Johnson_row").style.color = "#ffffff80";
 
   document.getElementById("Vallas_row").style.fontWeight = "normal";
-  document.getElementById("Vallas_block").style.backgroundColor = "#3490dc99";
+  document.getElementById("Vallas_block").style.backgroundColor = "#f66d9b99";
   document.getElementById("Vallas_row").style.color = "#ffffff80";
-
-  document.getElementById("Lightfoot_row").style.fontWeight = "normal";
-  document.getElementById("Lightfoot_block").style.backgroundColor =
-    "#6574cd99";
-  document.getElementById("Lightfoot_row").style.color = "#ffffff80";
-
-  document.getElementById("Sawyer_row").style.fontWeight = "normal";
-  document.getElementById("Sawyer_block").style.backgroundColor = "#9561e299";
-  document.getElementById("Sawyer_row").style.color = "#ffffff80";
-
-  document.getElementById("Garcia_row").style.fontWeight = "normal";
-  document.getElementById("Garcia_block").style.backgroundColor = "#f66d9b99";
-  document.getElementById("Garcia_row").style.color = "#ffffff80";
-};
+}
